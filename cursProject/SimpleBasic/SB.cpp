a70a1fac2f0982d3c130c68685418cb7a78d990f
Veltare <Veltare@gmail.com>
2018-05-17 15:26:04 +0600

    Add New Algorithm Arithmetic Sequence Transformation,Delete OLD Alg...

--- cursProject/SimpleBasic/SB.cpp
+++ cursProject/SimpleBasic/SB.cpp
@@ -33,7 +33,10 @@ vector<string>stringSplit(string line);
 int existenceVariable(string A);
 int FindVariable(string A);
 int OperationProcess(string operation,int index1,int index2,int index3,string cell,string &value,string&command);
-
+vector<string> ReverseRecordingCreation(vector<string> line);
+int OperationPriority(string operation);
+bool IsOperation(string operation);
+void ResolutionProcess(vector<string> lineA,string &value,string&command);
 
 main(int argc, char* argv[])
 {
@@ -230,133 +233,21 @@ string analysisCommand(string line,vector<string> &lineArray)
     else
     if(line == "LET")
         {
-             
-             if(existenceVariable(lineArray[2])!=-1)
-                {
-                   
-                   for(int i = 0;i < lineArray.size()-1;i++)
-                   {
-                       
-                       if(lineArray[i] == "(")
-                       {
-                          vector <string> temp;
-                           int m = i+1;
-                           
-                           while(lineArray[m]!=")")
-                           {
-                               if(m > lineArray.size()-1)
-                                  return "-1";
-                                temp.push_back(lineArray[m]);
-                                m++;
-                           }
-                           if(analysisCommand(line,temp)=="-1")
-                           {
-                                //for(int i = 0;i < temp.size();i++)
-                                  //             cout<<temp[i]<<endl;
-                               cout<<"ZAHOD"<<endl;
-                               int c = i+1;
-                               
-                               while(c!=m)
-                               {
-                                   lineArray.erase(lineArray.begin()+i+1);
-                                   c++;
-                               }
-                               for(int k = 0;k < temp.size();k++)
-                                lineArray.insert(lineArray.begin()+i+1,temp[k]);
-                                lineArray.erase(lineArray.begin()+i); 
-                                lineArray.erase(lineArray.begin()+i+1);    
-                                for(int i = 0;i < lineArray.size();i++)
-                                    cout<<lineArray[i];
-                               cout<<endl;  
-                           }
-                           
-                           
-                       }
-                   }
-                   for(int i = lineArray.size()-1;i >= 0;i--)
-                   {
-                    if(i-1 >= 0 && i+1 < lineArray.size())
-                       if(existenceVariable(lineArray[i-1])!=-1 && existenceVariable(lineArray[i+1])!=-1 )
-                       {
-                          
-                           if(lineArray[i] == "*" || lineArray[i] == "/")
-                           {
-                              
-                            OperationProcess(lineArray[i],existenceVariable(lineArray[i-1]),existenceVariable(lineArray[i+1]),
-                            existenceVariable(lineArray[i-1]),cell,value,command);
-                            lineArray.erase(lineArray.begin()+i);
-                            lineArray.erase(lineArray.begin()+i);
-                            lineArray[i-1] = GlobalVariable[GlobalVariable.size()-1].name;
-                            
-                                     
-                           }
-                           else
-                           {
-                               PriorityOperation = true;
-                           }
-                        
-                           
-                       }
-                   }
-                   for(int i = 0;i < lineArray.size();i++)
-                                    cout<<lineArray[i];
-                               cout<<endl; 
-                   for(int i = lineArray.size()-1;i >= 0;i--)
-                   {
-                       
-                    if(i-1 >= 0 && i+1 < lineArray.size())
-                       if(existenceVariable(lineArray[i-1])!=-1 && existenceVariable(lineArray[i+1])!=-1 )
-                       {
-                           
-                            
-                           if(lineArray[i] == "+" || lineArray[i] == "-")
-                           {
-                             
-                            OperationProcess(lineArray[i],existenceVariable(lineArray[i-1]),existenceVariable(lineArray[i+1]),
-                            existenceVariable(lineArray[i-1]),cell,value,command);
-                            lineArray.erase(lineArray.begin()+i);
-                            lineArray.erase(lineArray.begin()+i);
-                           
-                            lineArray[i-1] = GlobalVariable[GlobalVariable.size()-1].name;
-                                        
-                           }
-                           else
-                           {
-                               PriorityOperation = true;
-                           }
-                          
-                       }
-                   }
-                   for(int i = 0;i < lineArray.size();i++)
-                                    cout<<lineArray[i];
-                               cout<<endl; 
-                   for(int i = 0;i < lineArray.size();i++)
-                   {
-                        if(lineArray[i] == "=")
-                           {
-                            
-                            
-                            command = "LOAD";
-                            value =  GlobalVariable[existenceVariable(lineArray[i+1])].mem_address;
-                            cell = to_string(numericMemory);
-                            compliteString=cell+" "+command+" "+value;
-                            GlobalString.push_back(compliteString);
-                            numericMemory++;
-
-                            command = "STORE";
-                            value =  GlobalVariable[existenceVariable(lineArray[2])].mem_address;
-                            cell = to_string(numericMemory);
-                            compliteString=cell+" "+command+" "+value;
-                            GlobalString.push_back(compliteString);
-                            numericMemory++;   
-
-                           }
-                   }
-                   
-                }
-                
+             vector<string> T = lineArray;
+             while(T[0]!= "LET")
+             {
+                 T.erase(T.begin());
+             }
+             T.erase(T.begin());
+             T = ReverseRecordingCreation(T);
+             for(int z = 0;z<T.size();z++)
+             {
+                 cout<<T[z];
+             }
+             cout<<endl;
+             ResolutionProcess(T,value,command);   
                  return "-1";
-                }
+        }
                 
     else  
     if(line == "GOTO")
@@ -443,29 +334,45 @@ int existenceVariable(string A)
     }
    return -1;
 }
-int OperationProcess(string operation,int index1,int index2,int index3,string cell,string &value,string&command)
+int OperationProcess(string operation,Variable V1,Variable V2,Variable V3,string cell,string &value,string&command)
 {
 
  
     string compliteString;
    
     string operand;
-    if(IsFirst)
+    if(operation == "=")
     {
         command = "LOAD";
-        value =  GlobalVariable[index1].mem_address;
+        value =  V1.mem_address;
+        cell = to_string(numericMemory);
+        compliteString=cell+" "+command+" "+value;
+        GlobalString.push_back(compliteString);
+        numericMemory++;
+        IsFirst = false;    
+        command = "STORE";
+        value =  V2.mem_address;
+        cell = to_string(numericMemory);
+        compliteString=cell+" "+command+" "+value;
+        GlobalString.push_back(compliteString);
+        numericMemory++;
+    return 0;
+    
+    }
+        command = "LOAD";
+        value =  V1.mem_address;
         cell = to_string(numericMemory);
         compliteString=cell+" "+command+" "+value;
         GlobalString.push_back(compliteString);
         numericMemory++;
         IsFirst = false;
-    }
+    
   
     if(operation == "-")
     {
     
     command = "SUB";
-    value =  GlobalVariable[index2].mem_address;
+    value =  V2.mem_address;
     cell = to_string(numericMemory);
     compliteString=cell+" "+command+" "+value;
     GlobalString.push_back(compliteString);
@@ -475,7 +382,7 @@ int OperationProcess(string operation,int index1,int index2,int index3,string ce
     if(operation == "+")
     {
     command = "ADD";
-    value =  GlobalVariable[index2].mem_address;
+    value =  V2.mem_address;
     cell = to_string(numericMemory);
     compliteString=cell+" "+command+" "+value;
     GlobalString.push_back(compliteString);
@@ -486,7 +393,7 @@ int OperationProcess(string operation,int index1,int index2,int index3,string ce
     if(operation == "*")
     {
     command = "MUL";
-    value =  GlobalVariable[index2].mem_address;
+    value =  V2.mem_address;
     cell = to_string(numericMemory);
     compliteString=cell+" "+command+" "+value;
     GlobalString.push_back(compliteString);
@@ -497,7 +404,7 @@ int OperationProcess(string operation,int index1,int index2,int index3,string ce
     {
   
     command = "DIVIDE";
-    value =  GlobalVariable[index2].mem_address;
+    value =  V2.mem_address;
     cell = to_string(numericMemory);
     compliteString=cell+" "+command+" "+value;
     GlobalString.push_back(compliteString);
@@ -505,6 +412,7 @@ int OperationProcess(string operation,int index1,int index2,int index3,string ce
    
     
     }
+    
     if(PriorityOperation == true)
     {
          
@@ -516,7 +424,7 @@ int OperationProcess(string operation,int index1,int index2,int index3,string ce
         temp->mem_address = cell;
                    
         temp->value = 0;
-        operand = GlobalVariable[index3].name+cell;
+        operand = V3.name+cell;
         temp->name = operand;
         GlobalVariable.push_back(*temp);
          
@@ -532,3 +440,129 @@ int OperationProcess(string operation,int index1,int index2,int index3,string ce
     return 0;
 }
 
+vector<string> ReverseRecordingCreation(vector<string> line)
+{
+    vector<string> input;
+    vector<string> operation;
+    for(int i = 0; i < line.size();i++)
+        {
+            if(line[i] == "(")
+            {
+                operation.push_back(line[i]);
+                continue;
+            }
+            if(line[i] == ")")
+                {
+                    
+                    for(int k = 0;k<operation.size();k++)
+                    {
+                        if(operation[k] == "(")
+                        {   operation.erase(operation.begin() + k);
+                            
+                            
+                            for(int m = operation.size()-1;m>=k;m--)
+                            {
+                                //cout<<operation[m]<<endl;  
+                                input.push_back(operation[m]);
+                                if(k + 1 < operation.size())
+                                    operation.erase(operation.begin() + k+1);
+                                else    
+                                if(k < operation.size())
+                                    operation.erase(operation.begin() + k);
+                            }
+                            continue;
+                        }
+                    }
+                    continue;
+                } 
+            if(IsOperation(line[i]))
+            {
+                
+                
+                if(operation.size() > 0)
+                    if(OperationPriority(operation[operation.size() - 1]) >= OperationPriority(line[i]) && operation[operation.size() - 1] != "(")
+                    {
+                        /*cout<<"COMPLITE"<<endl;
+                        cout<<OperationPriority(operation[operation.size() - 1]) << ">="<<OperationPriority(line[i])<<endl;
+                        cout<<"DO"<<endl;
+                        cout<<"value"<<line[i]<<endl;
+                        for(int i = 0; i < operation.size();i++)
+                                cout<<operation[i];
+                                cout<<endl;*/
+                        input.push_back(operation[operation.size() - 1]);
+                        operation.erase(operation.begin() + (operation.size() - 1));
+                        //cout<<"POSLE"<<endl;
+                        
+                        operation.push_back(line[i]);
+                        /*for(int i = 0; i < operation.size();i++)
+                                cout<<operation[i];
+                                cout<<endl;*/
+                        continue;
+                    }
+            
+            operation.push_back(line[i]);
+            }
+            else
+            {
+                
+                input.push_back(line[i]);
+            }
+        }
+       
+         for(int i = operation.size()-1; i >= 0;i--)
+            input.push_back(operation[i]);
+        
+        return input;    
+}
+int OperationPriority(string operation)
+{
+    if(operation == "+" || operation == "-" )
+        return 2;
+    if(operation == "/" || operation == "*" )
+        return 3;
+    if(operation == "(" || operation == ")" )
+        return 1; 
+    if(operation == "=")
+        return 0;
+    return -1;    
+}
+bool IsOperation(string operation)
+{
+    
+    if(operation == "+" || operation == "-" || operation == "/" || operation == "*" || operation == "=")
+        return true;
+    else 
+        return false;    
+}
+void ResolutionProcess(vector<string> lineA,string &value,string&command)
+{
+    vector<Variable> V;
+  
+    string cell;
+    
+    value = to_string(numericMemory+1);
+    cell = to_string(numericMemory);
+    for(int i = 0;i<lineA.size();i++)
+    {
+         if(!IsOperation(lineA[i]))
+         {
+             if(existenceVariable(lineA[i])!=-1)
+             {
+                 
+                 V.push_back(GlobalVariable[existenceVariable(lineA[i])]);
+             }
+         }
+         else
+         {
+             if(V.size() > 1)
+             {
+                 OperationProcess(lineA[i],V[V.size() - 2],V[V.size() - 1],
+                            V[V.size() - 2],cell,value,command);
+                 V.erase(V.end());
+                 V.erase(V.end());           
+                 V.push_back(GlobalVariable[GlobalVariable.size()-1]);           
+             }
+         }
+    }
+    
+}
